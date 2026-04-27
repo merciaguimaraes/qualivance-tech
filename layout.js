42596b3778a96b73caa3868e84fac7f9a66290e2
merciaguimaraes <80049713+merciaguimaraes@users.noreply.github.com>
2026-04-27 14:35:40 -0300

    Update layout.js

--- layout.js
+++ layout.js
@@ -1,1 +1,12 @@
-export const metadata={title:'Qualivance Tech',description:'Qualidade que impulsiona resultados.'};export default function RootLayout({children}){return <html lang='pt-br'><body>{children}</body></html>}
+export const metadata = {
+  title: "Qualivance Tech",
+  description: "Qualidade que impulsiona resultados."
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-br">
+      <body>{children}</body>
+    </html>
+  )
+}
