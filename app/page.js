8b59ea255acfad51f42912d8f88bf398b9c7ade9
merciaguimaraes <80049713+merciaguimaraes@users.noreply.github.com>
2026-04-28 14:19:10 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -27,7 +27,7 @@ export default function Home() {
         <div>
           {/* LOGO - usa extensão jpg/png/jpeg automática */}
           <img
-            src="https://i.postimg.cc/Ss4YD1PT/Design-sem-nome-(20).png"
+            src="https://i.postimg.cc/jj27D39q/Design-sem-nome-(21).png"
             alt="Qualivance Tech"
             style={{
               width: "100%",
